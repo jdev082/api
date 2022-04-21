8f642ba1b98904cd569612dd8d3545b14feb0695
lankybox02 <104015308+lankybox02@users.noreply.github.com>
2022-04-21 12:30:13 +0300

    Grammar

--- data/facts.js
+++ data/facts.js
@@ -1,6 +1,6 @@
 let facts = [
   // add facts here, this is the data, this is the database. Remember, each fact is to be put in quotation marks ("quote") and have a comma (,) at the end
- "JavaScript took only 10 days to develop",
+ "JavaScript only took 10 days to develop",
  "A banana is a berry, but a strawberry isn't",
  "According to Github, javaScript is the most used language in the world",
  "There are around 700 separate programming languages",
